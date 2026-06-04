30ab1c4232bd94c3d32e903bcc2ea62e993b22cd
Monica Hanson <mhanson@monicarose.tech>
2026-06-04 14:31:58 -0500

    initial additional api variant support for kea dhcpv4
    Signed-off-by: Monica Hanson <mhanson@monicarose.tech>

--- opnsense/kea_dhcpv4.go
+++ opnsense/kea_dhcpv4.go
@@ -1,5 +1,10 @@
 package opnsense
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type KeaDhcpv4LeasesRow struct {
 	If                   string   `json:"if"`
 	Address              string   `json:"address"`
@@ -18,6 +23,60 @@ type KeaDhcpv4LeasesRow struct {
 	UserContext          string   `json:"user_context"`
 	SubnetId             int      `json:"subnet_id"`
 	PoolId               int      `json:"pool_id"`
+}
+
+type KeaDhcpv4LeasesRowAllStrings struct {
+	If                   string `json:"if"`
+	Address              string `json:"address"`
+	Hwaddr               string `json:"hwaddr"`
+	ClientId             string `json:"client_id"`
+	ValidLifetime        string `json:"valid_lifetime"`
+	Expiration           string `json:"expire"`
+	InterfaceDescription string `json:"if_descr"`
+	InterfaceName        string `json:"if_name"`
+	MacInfo              string `json:"mac_info"`
+	IsReserved           string `json:"is_reserved"`
+	Hostname             string `json:"hostname"`
+	FqdnForward          string `json:"fqdn_fwd"`
+	FqdnReceived         string `json:"fqdn_rev"`
+	State                string `json:"state"`
+	UserContext          string `json:"user_context"`
+	SubnetId             string `json:"subnet_id"`
+	PoolId               string `json:"pool_id"`
+}
+
+type KeaDhcpv4LeasesRowIntString struct {
+	If                   string `json:"if"`
+	Address              string `json:"address"`
+	Hwaddr               string `json:"hwaddr"`
+	ClientId             string `json:"client_id"`
+	ValidLifetime        int    `json:"valid_lifetime"`
+	Expiration           int    `json:"expire"`
+	InterfaceDescription string `json:"if_descr"`
+	InterfaceName        string `json:"if_name"`
+	MacInfo              string `json:"mac_info"`
+	IsReserved           string `json:"is_reserved"`
+	Hostname             string `json:"hostname"`
+	FqdnForward          string `json:"fqdn_fwd"`
+	FqdnReceived         string `json:"fqdn_rev"`
+	State                int    `json:"state"`
+	UserContext          string `json:"user_context"`
+	SubnetId             int    `json:"subnet_id"`
+	PoolId               int    `json:"pool_id"`
+}
+
+type KeaDhcpv4LeasesResponseAllStrings struct {
+	Total    int `json:"total"`
+	RowCount int `json:"rowCount"`
+	Current  int `json:"current"`
+	Rows     []KeaDhcpv4LeasesRowAllStrings
+}
+
+type KeaDhcpv4LeasesResponseIntString struct {
+	Total    int `json:"total"`
+	RowCount int `json:"rowCount"`
+	Current  int `json:"current"`
+	Rows     []KeaDhcpv4LeasesRowIntString
 }
 
 type KeaDhcpv4LeasesResponse struct {
@@ -48,6 +107,97 @@ type KeaDhcpv4Leases struct {
 	ReservedLeaseCount map[string]int
 	LeaseCount         map[string]int
 	Interfaces         map[string]KeaDhcpV4InterfaceInfo
+}
+
+func parseDHCPv4LeasesAllStrings(response KeaDhcpv4LeasesResponseAllStrings) (KeaDhcpv4Leases, *APICallError) {
+	data := KeaDhcpv4Leases{}
+	data.Interfaces = make(map[string]KeaDhcpV4InterfaceInfo)
+	data.LeaseCount = make(map[string]int)
+	data.ReservedLeaseCount = make(map[string]int)
+
+	for _, row := range response.Rows {
+		// Update total reservation count
+		data.LeaseCount[row.InterfaceName] += 1
+
+		// Update reservation count
+		if len(row.IsReserved) > 0 {
+			data.ReservedLeaseCount[row.InterfaceName] += 1
+		}
+
+		expiration, err := strconv.Atoi(row.Expiration)
+		if err != nil {
+			return data, &APICallError{
+				Endpoint:   "keaDhcpv4",
+				Message:    "failed to parse expiration",
+				StatusCode: 0,
+			}
+		}
+		lifetime, err := strconv.Atoi(row.ValidLifetime)
+		if err != nil {
+			return data, &APICallError{
+				Endpoint:   "keaDhcpv4",
+				Message:    "failed to parse valid lifetime",
+				StatusCode: 0,
+			}
+		}
+		lease := KeaDhcpv4Lease{
+			InterfaceName: row.InterfaceName,
+			Hostname:      row.Hostname,
+			Address:       row.Address,
+			Mac:           row.Hwaddr,
+			ClientId:      row.ClientId,
+			MacInfo:       row.MacInfo,
+			Expiration:    expiration,
+			ValidLifetime: lifetime,
+		}
+
+		// Add the information in
+		data.Leases = append(data.Leases, lease)
+
+		data.Interfaces[row.InterfaceName] = KeaDhcpV4InterfaceInfo{
+			Name:        row.If,
+			Description: row.InterfaceDescription,
+		}
+	}
+
+	return data, nil
+}
+
+func parseDHCPv4LeasesStringInt(response KeaDhcpv4LeasesResponseIntString) (KeaDhcpv4Leases, *APICallError) {
+	data := KeaDhcpv4Leases{}
+	data.Interfaces = make(map[string]KeaDhcpV4InterfaceInfo)
+	data.LeaseCount = make(map[string]int)
+	data.ReservedLeaseCount = make(map[string]int)
+
+	for _, row := range response.Rows {
+		// Update total reservation count
+		data.LeaseCount[row.InterfaceName] += 1
+
+		// Update reservation count
+		if len(row.IsReserved) > 0 {
+			data.ReservedLeaseCount[row.InterfaceName] += 1
+		}
+		lease := KeaDhcpv4Lease{
+			InterfaceName: row.InterfaceName,
+			Hostname:      row.Hostname,
+			Address:       row.Address,
+			Mac:           row.Hwaddr,
+			ClientId:      row.ClientId,
+			MacInfo:       row.MacInfo,
+			Expiration:    row.Expiration,
+			ValidLifetime: row.ValidLifetime,
+		}
+
+		// Add the information in
+		data.Leases = append(data.Leases, lease)
+
+		data.Interfaces[row.InterfaceName] = KeaDhcpV4InterfaceInfo{
+			Name:        row.If,
+			Description: row.InterfaceDescription,
+		}
+	}
+
+	return data, nil
 }
 
 func parseDHCPv4Leases(response KeaDhcpv4LeasesResponse) (KeaDhcpv4Leases, *APICallError) {
@@ -92,7 +242,10 @@ func parseDHCPv4Leases(response KeaDhcpv4LeasesResponse) (KeaDhcpv4Leases, *APIC
 
 func (c *Client) FetchLeasesv4() (KeaDhcpv4Leases, *APICallError) {
 	var resp KeaDhcpv4LeasesResponse
+	var allStringsResp KeaDhcpv4LeasesResponseAllStrings
+	var stringIntResp KeaDhcpv4LeasesResponseIntString
 	var data KeaDhcpv4Leases
+	apiVariant := 0
 
 	url, ok := c.endpoints["keaDhcpv4"]
 	if !ok {
@@ -105,10 +258,30 @@ func (c *Client) FetchLeasesv4() (KeaDhcpv4Leases, *APICallError) {
 
 	err := c.do("GET", url, nil, &resp)
 	if err != nil {
-		return data, err
+		apiVariant += 1
+		err = c.do("GET", url, nil, &allStringsResp)
+		if err != nil {
+			apiVariant += 1
+			err = c.do("GET", url, nil, &stringIntResp)
+			if err != nil {
+				return data, err
+			}
+		}
 	}
 
-	data, err = parseDHCPv4Leases(resp)
+	if apiVariant == 0 {
+		data, err = parseDHCPv4Leases(resp)
+	} else if apiVariant == 1 {
+		data, err = parseDHCPv4LeasesAllStrings(allStringsResp)
+	} else if apiVariant == 2 {
+		data, err = parseDHCPv4LeasesStringInt(stringIntResp)
+	} else {
+		err = &APICallError{
+			Endpoint:   "keaDhcpv4",
+			Message:    fmt.Sprintf("unknown api variant %d", apiVariant),
+			StatusCode: 0,
+		}
+	}
 	if err != nil {
 		return data, err
 	}
